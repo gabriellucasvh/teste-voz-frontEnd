d95696981aca9762111bd3a854f9b3b4a5e1c205
Gabriel Gonçalves <gabriel.gopp@gmail.com>
2025-01-08 02:43:03 -0300

    Responsividade aplicada

--- components/sections/Inicio.tsx
+++ components/sections/Inicio.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Inicio = () => {
   return (
-    <section className='bg-no-repeat bg-top bg-contain lg:bg-cover w-full h-[30vh] lg:h-[110vh]' style={{backgroundImage: 'url(/fundo.svg)'}}>
+    <section className='bg-no-repeat bg-top bg-contain md:bg-cover w-full h-[30vh] md:h-[70vh] lg:h-[110vh]' style={{backgroundImage: 'url(/fundo.svg)'}}>
       {/* Aqui está definido o fundo principal do menu */}
     </section>
   );
